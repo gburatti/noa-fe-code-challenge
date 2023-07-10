a45322cd5ac4002060eca121fe412863eca3c94f
Raheem Adebayo <adraheemzy@gmail.com>
2023-07-10 14:17:47 +0100

    distance calculation

--- src/pages/MapPage/index.tsx
+++ src/pages/MapPage/index.tsx
@@ -3,6 +3,7 @@ import {MapContainer, Marker, Polyline, TileLayer, Tooltip} from "react-leaflet"
 import useAppContext from "../../context/useAppContext";
 import {LatLngExpression} from "leaflet";
 import styles from "./styles.module.css"
+import geolocationService from "../../services/geolocation.service";
 
 interface IProps {
 }
@@ -24,6 +25,10 @@ function MapPage(props: IProps) {
       return [0, 0]
     }
   }, [ipInfo?.loc])
+
+  const distance = useMemo(() => {
+    return geolocationService.calculateDistance(currentLocation[0], currentLocation[1], ipLocation[0], ipLocation[1]);
+  }, [currentLocation, ipLocation])
 
   return (
     <div>
@@ -60,7 +65,7 @@ function MapPage(props: IProps) {
                   currentLocation, ipLocation
                 ]}
               >
-                <Tooltip sticky>Distance</Tooltip>
+                <Tooltip sticky>Distance: {distance} km</Tooltip>
               </Polyline>
             </MapContainer>
           </div>
--- src/services/geolocation.service.ts
+++ src/services/geolocation.service.ts
@@ -39,6 +39,29 @@ class GeolocationService {
     })
   }
 
+  public calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number) {
+    const earthRadius = 6371;
+
+    const latRad1 = this.degToRad(lat1);
+    const lonRad1 = this.degToRad(lon1);
+    const latRad2 = this.degToRad(lat2);
+    const lonRad2 = this.degToRad(lon2);
+
+    const latDiff = latRad2 - latRad1;
+    const lonDiff = lonRad2 - lonRad1;
+
+    const a =
+      Math.sin(latDiff / 2) ** 2 +
+      Math.cos(latRad1) * Math.cos(latRad2) * Math.sin(lonDiff / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = earthRadius * c;
+    return distance.toFixed(4);
+  }
+
+  private degToRad(degrees: number) {
+    return degrees * (Math.PI / 180);
+  }
+
 }
 
 const geolocationService = new GeolocationService();
